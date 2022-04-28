4dced5f7918f4e931d1400ab6b45519229ae6ab2
Samuel Geissinger <samuelmgeissinger@gmail.com>
2022-04-28 12:33:00 -0600

    added rollbar

--- server/index.js
+++ server/index.js
@@ -2,9 +2,19 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+const Rollbar = require("rollbar");
+const rollbar = new Rollbar({
+  accessToken: "d7195402a8f64376a3281978aefb76b0",
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+});
+
+rollbar.log("Hello world!");
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../index.html"));
 });
+
 const port = process.env.PORT || 5040;
 
 app.listen(port, () => console.log(`listening on port ${port}`));
