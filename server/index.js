b34d7b952d44d83a12f79d079c47d3de90c9fcc9
Samuel Geissinger <samuelmgeissinger@gmail.com>
2022-04-28 12:42:47 -0600

    added more functionality

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-
+app.use(express.json());
 const Rollbar = require("rollbar");
 const rollbar = new Rollbar({
   accessToken: "d7195402a8f64376a3281978aefb76b0",
@@ -11,10 +11,24 @@ const rollbar = new Rollbar({
 
 rollbar.log("Hello world!");
 
+const students = [];
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../index.html"));
 });
 
-const port = process.env.PORT || 5040;
+app.post("/api/student", (req, res) => {
+  const { name } = req.body;
+  name = name.trim();
 
+  students.push(name);
+  rollbar.log("Student added successfully", {
+    author: "Scott",
+    type: "manual entry",
+  });
+  res.status(200).send(students);
+});
+
+const port = process.env.PORT || 5040;
+app.use(rollbar.errorHandler());
 app.listen(port, () => console.log(`listening on port ${port}`));
